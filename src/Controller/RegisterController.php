29919cbfcd48dba300a5a7caa68e0879f02d1c46
Ourtof <yann.eysseric@egs.school>
2025-08-06 03:49:57 +0200

    refacto registerController

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -27,7 +27,7 @@ public function register(
     }
 
     // 2. On vérifie la présence des champs obligatoires
-    $requiredFields = ['email', 'password', 'pseudo', 'prenom', 'nom', 'date_naissance', 'adresse', 'ville', 'code_postal', 'adresse_mail'];
+    $requiredFields = ['email', 'password', 'pseudo', 'prenom', 'nom', 'dateNaissance', 'adresse', 'ville', 'codePostal'];
     foreach ($requiredFields as $field) {
         if (empty($data[$field])) {
             return new JsonResponse(['error' => "Le champ '$field' est manquant"], Response::HTTP_BAD_REQUEST);
