d9f99a7586a4e9027bd2c74e167376d7e0795005
Ourtof <yann.eysseric@egs.school>
2025-09-11 13:23:33 +0200

    temp inscription/connexion à continuer

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/api')]
+class UserController extends AbstractController
+{
+    #[Route('/me', name: 'api_me', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function me(): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $this->json([
+            'id' => $user->getId(),
+            'email' => $user->getUserIdentifier(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
