9cf5a0ee29e6f18fee0a98eabf962f7b10cef46b
Ourtof <yann.eysseric@egs.school>
2025-11-05 19:00:48 +0100

    👌 IMPROVE: detail - renommage route userController

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/api')]
 class UserController extends AbstractController
 {
-    #[Route('/me', name: 'api_me', methods: ['GET'])]
+    #[Route('/user', name: 'api_user', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     public function me(): JsonResponse
     {
